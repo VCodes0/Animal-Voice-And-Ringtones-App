37b3c83a751665989aa96355f624f4117ce7c881
VCodes0 <orewavishnuprajapati@gmail.com>
2025-07-09 18:27:53 +0530

    Ass All Providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,38 @@
+import 'package:animal_voice_app/providers/animal_caegory_provider.dart';
+import 'package:animal_voice_app/providers/birds_provider.dart';
+import 'package:animal_voice_app/providers/farm_provider.dart';
+import 'package:animal_voice_app/providers/insect_provider.dart';
+import 'package:animal_voice_app/providers/land_animals_provider.dart';
+import 'package:animal_voice_app/providers/mammals_provider.dart';
+import 'package:animal_voice_app/providers/pet_animals_povider.dart';
+import 'package:animal_voice_app/providers/reptiles_and_amphibians_providers.dart';
+import 'package:animal_voice_app/providers/water_animals_provider.dart';
+import 'package:animal_voice_app/providers/wild_animals_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:provider/provider.dart';
 
-void main(){
-  runApp(const MyApp());
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AnimalCategoryProvider()),
+        ChangeNotifierProvider(create: (context) => WaterAnimalsProvider()),
+        ChangeNotifierProvider(create: (context) => WildAnimalsProvider()),
+        ChangeNotifierProvider(create: (context) => LandAnimalsProvider()),
+        ChangeNotifierProvider(create: (context) => FarmAnimalProvider()),
+        ChangeNotifierProvider(create: (context) => PetAnimalsPovider()),
+        ChangeNotifierProvider(create: (context) => PetAnimalsPovider()),
+        ChangeNotifierProvider(create: (context) => InsectsProvider()),
+        ChangeNotifierProvider(create: (context) => MammalsProvider()),
+        ChangeNotifierProvider(create: (context) => BirdsProvider()),
+        ChangeNotifierProvider(
+          create: (context) => ReptilesAndAmphibiansProvider(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -9,7 +40,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: "Animal Voice And Ringtone App",
       debugShowCheckedModeBanner: false,
       home: Scaffold(),
